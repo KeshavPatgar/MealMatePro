7fbd7499ba0f0a8e2fc9a8061b5b64c622c4bc38
KeshavPatgar <keshavpatagar86@gmail.com>
2026-05-25 20:21:23 +0530

    Updated picture field

--- mealmatepro/settings.py
+++ mealmatepro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n+fplp^a34fc*m$4y0n^i1+5(fnw&ty3%4ue+^k5%hd^hrdhsp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
