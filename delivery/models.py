2249a9e09065eb4c9c33acae8463aaf7fadbbebd
KeshavPatgar <keshavpatagar86@gmail.com>
2026-05-25 18:28:43 +0530

    PostgreSQL database connected

--- delivery/models.py
+++ delivery/models.py
@@ -4,11 +4,11 @@ from django.db import models
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-    email = models.CharField(max_length = 20)
-    mobile = models.CharField(max_length = 10)
-    address = models.CharField(max_length = 50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
 
     def __str__(self):
         return self.username
